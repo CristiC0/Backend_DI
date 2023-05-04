2e7d8c52aa37bbf4e8c0d9432324ec6b9e395240
CristiC0 <82097873+CristiC0@users.noreply.github.com>
2023-05-04 17:07:51 +0300

    Created upload teacher image

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
-import { Module, Logger } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/roles.guard';
 import { TeachersModule } from './teachers/teachers.module';
+import { UploadModule } from './upload/upload.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, TeachersModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UsersModule,
+    TeachersModule,
+    UploadModule,
+    MulterModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'files'),
+    }),
+  ],
   providers: [
     {
       provide: APP_GUARD,
