ac9424b592fb2d1bff5dd7f37c77adf1fd102b29
Cristi Crudu <cristicrudu2002@gmail.com>
2023-05-28 22:27:39 +0300

    fixed typo

--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -157,7 +157,7 @@ export class TeachersService {
       const obj = {
         ...teacher,
         username: teacher.user.username,
-        lastname: teacher.user.lastName,
+        lastName: teacher.user.lastName,
         firstName: teacher.user.firstName,
         courses: teacher.courses.reduce(
           (courses, course) => [...courses, course.course],
