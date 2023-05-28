00868a57b6ac4c2608ff456bd9346f626dbf169a
Cristi Crudu <cristicrudu2002@gmail.com>
2023-05-28 21:39:30 +0300

    added names to update teacher dto

--- src/teachers/dto/update-teacher.dto.ts
+++ src/teachers/dto/update-teacher.dto.ts
@@ -1,4 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTeacherDto } from './create-teacher.dto';
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+const nameRegex = /^[\w- ]*$/;
 
-export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {}
+export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Matches(nameRegex, {
+    message: 'Only letters. Spaces and hythens are accepted',
+  })
+  @Length(3, 16, { message: 'Between 3 and 16 characters' })
+  public firstName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Matches(nameRegex, {
+    message: 'Only letters. Spaces and hythens are accepted',
+  })
+  @Length(3, 16, { message: 'Between 3 and 16 characters' })
+  public lastName: string;
+}
