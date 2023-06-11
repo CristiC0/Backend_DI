488d548af7d08126ba6c6e01eb84cbd2bb451a5f
Cristi Crudu <cristicrudu2002@gmail.com>
2023-06-11 07:07:35 +0300

    upload course image

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -102,4 +102,49 @@ export class UploadController {
       imagePath: `http://localhost:3000/images/news/${file.filename}`,
     };
   }
+
+  @ApiResponse({
+    status: 400,
+    type: JSON.stringify({ photo: 'url' }),
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'When file not is bigger than 5MB or not in jpg,jpeg or png format ',
+  })
+  @Post('image/courses')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'files/images/courses',
+        filename: (req, file, callback) => {
+          if (!file.originalname.match(/\.(jpg|jpeg|png)$/))
+            callback(
+              new BadRequestException(
+                'Accepted formats are only jpg,jpeg and png',
+              ),
+              null,
+            );
+
+          if (file.size > 5_000_000)
+            callback(
+              new BadRequestException('File has to be maximum 5MB'),
+              null,
+            );
+          const filename = `${
+            file.originalname.split('.')[0]
+          }-${crypto.randomUUID()}${extname(file.originalname)}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  uploadCoursesImage(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    return {
+      photo: `http://localhost:3000/courses/news/${file.filename}`,
+    };
+  }
 }
